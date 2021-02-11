60d6a120ac6bf76b29b00961ea3195013d639c07
Limo Vincent <48591371+LimoVincent@users.noreply.github.com>
2021-02-11 09:38:39 +0200

    Who Let the Bugs Out??

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express')
-var router = express.Router()
 
+var router = express.Router()
+var coolRouter = require('./catalog')
+
+router.use('./cool', coolRouter)
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('Users')
